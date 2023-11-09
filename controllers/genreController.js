bfcc18e86816d615e6ec59f8719d8e20b4d96b72
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-11-09 20:07:18 +0000

    Create Genre Controller Methods
    Created list, detail, add_get and add_post methods.

---
+++ controllers/genreController.js
@@ -0,0 +1,54 @@
+const { validationResult } = require('express-validator');
+const Genre = require('../models/genre');
+const asyncHandler = require('express-async-handler');
+
+exports.genre_list = asyncHandler(async(req, res, next) => {
+    const allGenres = await Genre.find({}).exec();
+    res.render('genre_list', {
+        title: "All Genres",
+        genre_list: allGenres,
+    })
+})
+
+exports.genre_detail = asyncHandler(async(req, res, next) => {
+    const genre = await Genre.findById(req.params.id)
+    res.render('genre_detail', {
+        title: genre.name,
+        genre: genre,
+    })
+})
+
+exports.genre_add_get = asyncHandler(async(req, res, next) => {
+    res.render("genre_form", {
+        title: 'Add Genre'
+    })
+})
+
+exports.genre_add_post = [
+    body("name")
+    .trim()
+    .isLength({min: 1})
+    .escape()
+    .withMessage("Name required"),
+
+    asyncHandler(async(req, res, next) => {
+        const errors = validationResult(req)
+
+        const genre = new Genre({
+            name: req.body.name
+        })
+
+        if (!errors.isEmpty()) {
+            res.render('genre_form', {
+                title: genre.name,
+                genre: genre,
+                errors: errors.array(),
+            })
+        }
+        else {
+            await genre.save();
+            res.redirect(genre.url);
+        }
+
+})
+]
