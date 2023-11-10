405cd9ebe04ce8e0e3cc5b66e5aa3a010a59af0c
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-11-10 11:59:08 +0000

    Add Playlist Controller Callbacks To Routes

--- models/playlist.js
+++ models/playlist.js
@@ -7,7 +7,7 @@ const PlaylistSchema = new Schema({
 })
 
 PlaylistSchema.virtual('url').get(function() {
-    return `/home/playlists/${._id}`;
+    return `/home/playlists/${this._id}`;
 })
 
 module.exports = mongoose.model('Playlist', PlaylistSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const song_controller = require("../controllers/songController");
 const artist_controller = require("../controllers/artistController");
 const genre_controller = require("../controllers/genreController");
+const playlist_controller = require("../controllers/playlistController");
+
 
 var express = require('express');
 var router = express.Router();
@@ -25,5 +27,11 @@ router.get('/home/genres/add', genre_controller.genre_add_get);
 router.post('/home/genres/add', genre_controller.genre_add_post);
 router.get('/home/genres/:id', genre_controller.genre_detail);
 router.get('/home/genres', genre_controller.genre_list);
+
+// Playlist Routes And Method
+router.get('/home/playlists/add', playlist_controller.playlist_add_get);
+router.post('/home/playlists/add', playlist_controller.playlist_add_post);
+router.get('/home/playlists/:id', playlist_controller.playlist_detail);
+router.get('/home/playlists/', playlist_controller.playlist_list);
 
 module.exports = router;
