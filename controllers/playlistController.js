196edc6ba9e2c48bf73e4adebde67fc28f2df402
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-11-10 11:45:18 +0000

    Create Playlist Controller Methods

---
+++ controllers/playlistController.js
@@ -0,0 +1,59 @@
+const Playlist = require('../models/playlist');
+
+const {validationResult, body} = require('express-validator');
+
+const asyncHandler = require('express-async-handler');
+const playlist = require('../models/playlist');
+
+exports.playlist_list = asyncHandler(async(req, res, next) => {
+    const allPlaylists = await Playlist.find({}).exec();
+    res.render('playlist_list', {
+        title: 'All Playlists',
+        playlist_list: allPlaylists,
+    })
+})
+
+exports.playlist_detail = asyncHandler(async(req, res, next) => {
+    const playlist = await Playlist.findById(req.params.id).exec();
+    res.render('playlist_detail', {
+        title: playlist.name,
+        playlist: playlist,
+    })
+})
+
+exports.playlist_add_get = asyncHandler(async(req, res, next) => {
+    res.render('playlist_form', {
+        title: 'Add Playlist',
+    })
+})
+
+exports.playlist_add_post = [
+    body("name")
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+
+
+
+    asyncHandler(async(req, res, next) => {
+
+        const errors = validationResult(req);
+
+        const playlist = new Playlist({
+            name: req.body.name
+        })
+
+        if (!errors.isEmpty()) {
+            res.render('playlist_list', {
+                title: 'Add Playlist',
+                playlist: playlist,
+                errors: errors.array(),
+            })
+        }
+
+        else {
+            await playlist.save();
+            res.redirect(playlist.url);
+        }
+    })
+]
