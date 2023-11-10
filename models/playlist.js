660b05217f26a30f8cadff3528b6154e056f7bd0
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-11-10 10:24:05 +0000

    Create Playlist Schema And Model

---
+++ models/playlist.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PlaylistSchema = new Schema({
+    title: {type: String, required: 'true'}
+})
+
+PlaylistSchema.virtual('url').get(function() {
+    return `/home/playlists/${._id}`;
+})
+
+module.exports = mongoose.model('Playlist', PlaylistSchema);
